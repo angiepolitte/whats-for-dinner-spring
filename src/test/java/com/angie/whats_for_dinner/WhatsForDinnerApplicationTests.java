f3cf1b8e03f648dac2a754362f39bbfade689e69
Angie <angiepolitte@gmail.com>
2025-03-04 10:14:23 -0600

    Initial Commit

---
+++ src/test/java/com/angie/whats_for_dinner/WhatsForDinnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angie.whats_for_dinner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsForDinnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
